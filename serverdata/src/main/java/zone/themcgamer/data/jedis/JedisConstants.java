ce4a6f759f61d08aac82b09355819cf4118e9391
Joel <unconfigured@null.spigotmc.org>
2021-05-23 12:16:01 +0200

    - Changed the database details

--- serverdata/src/main/java/zone/themcgamer/data/jedis/JedisConstants.java
+++ serverdata/src/main/java/zone/themcgamer/data/jedis/JedisConstants.java
@@ -5,6 +5,6 @@ package zone.themcgamer.data.jedis;
  */
 public class JedisConstants {
     public static final String HOST = "172.18.0.1";
-    public static final String AUTH = "CWhsuGvpYPhZt7ru";
+    public static final String AUTH = "CWhsuGvpYPhZ321dsaDAwqjk231_mgz";
     public static final int SELECTED_DB = 0;
 }
--- serverdata/src/main/java/zone/themcgamer/data/mysql/MySQLConstants.java
+++ serverdata/src/main/java/zone/themcgamer/data/mysql/MySQLConstants.java
@@ -7,5 +7,5 @@ public class MySQLConstants {
     public static final String HOST = "172.18.0.1";
 
     public static final String USERNAME = "mcgamerzone";
-    public static final String AUTH = "n3oCqfGeCG7lUcOlqvUG2JfyKMtEZakG0eNIA";
+    public static final String AUTH = "n3oCqfGeCG7lUcOlqvUG2JfyKMtEZakG0eNIA_mGz";
 }
